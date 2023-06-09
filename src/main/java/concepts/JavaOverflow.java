f9fcddd132c1e11516b4b75d8333a5b4702543dc
agarwal1995 <rahulagarwal199525@gmail.com>
2023-06-09 17:24:17 +0530

    Java Concept Doubt

---
+++ src/main/java/concepts/JavaOverflow.java
@@ -0,0 +1,20 @@
+package concepts;
+
+public class JavaOverflow {
+    public static void main(String[] args) {
+        int x = Integer.MAX_VALUE;
+        System.out.println(x);
+        x = x + 1;
+        System.out.println(x);
+        x= x +2;
+        System.out.println(x);
+
+        int y = Integer.MIN_VALUE;
+        System.out.println(y);
+        y = y-1;
+        System.out.println(y);
+        y=y-2;
+        System.out.println(y);
+
+    }
+}
