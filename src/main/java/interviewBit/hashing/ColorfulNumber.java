59655e7ac75d774f651902def8bd53d264ef0beb
agarwal1995 <rahulagarwal199525@gmail.com>
2023-06-09 17:55:29 +0530

    Add Comment ColorfulNumber

--- src/main/java/interviewBit/hashing/ColorfulNumber.java
+++ src/main/java/interviewBit/hashing/ColorfulNumber.java
@@ -5,7 +5,17 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * A number can be broken into different contiguous sub-subsequence parts.
+ */
 public class ColorfulNumber {
+    /**
+     * Convert the number to a list, iterate it over in nested loop (i:0-n,j:i-n), and find the product
+     * of each contiguous subarray and check in a hashset if it exists or not
+     *
+     * @param A number to check if that is colorful or not
+     * @return 1 if colorful and 0 if not
+     */
     public int colorful(int A) {
         List<Integer> list = new ArrayList<>();
         int n=0;
