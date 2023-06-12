db8d2c3c52cbd05c531648a5d0d7bc128af5baeb
agarwal1995 <rahulagarwal199525@gmail.com>
2023-06-12 08:20:42 +0530

    Added Comment in Two Sum

--- src/main/java/interviewBit/hashing/TwoSum.java
+++ src/main/java/interviewBit/hashing/TwoSum.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 /**
  * Given an array of integers, find two numbers such that they add up to a specific target number.
+ * If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, choose the one with minimum index1 out of them.
  */
 public class TwoSum {
 
