94c038d5243bac874005eb29b241d9bafa1c4bf2
agarwal1995 <rahulagarwal199525@gmail.com>
2024-08-26 19:36:20 +0530

    Update AddOneToNumber.java

--- src/main/java/interviewBit/arrays/AddOneToNumber.java
+++ src/main/java/interviewBit/arrays/AddOneToNumber.java
@@ -3,7 +3,21 @@ package interviewBit.arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Given a non-negative number represented as an array of digits,
+ * add 1 to the number ( increment the number represented by the digits ).
+ * The digits are stored such that the most significant digit is at the head of the list.
+ *
+ * @author raag
+ */
 public class AddOneToNumber {
+    /**
+     * Iterate the list from the end maintain carry 'i.e' sum/10,
+     * add the number to carry and store the result in a new list
+     * later iterate once more to check and remove any leading zeroes
+     * @param A list of input integers
+     * @return list of output with +1
+     */
     public ArrayList<Integer> plusOne(ArrayList<Integer> A) {
         int n= A.size()-1;
         int carry = 1;
