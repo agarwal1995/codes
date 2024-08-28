5b6bda4c0bd0d1bbfaf8ec366314beebba3f7346
agarwal1995 <rahulagarwal199525@gmail.com>
2024-08-28 10:43:46 +0530

    Update PickFromBothSides.java

--- src/main/java/interviewBit/arrays/PickFromBothSides.java
+++ src/main/java/interviewBit/arrays/PickFromBothSides.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+ * Given an integer array A of size N.
+ * You have to pick exactly B elements from either left or right end of the array A to get the maximum sum.
+ * Find and return this maximum possible sum.
+ */
 public class PickFromBothSides {
 
     public static void main(String[] args) throws Exception {
@@ -17,6 +22,16 @@ public class PickFromBothSides {
         System.out.println(pickFromBothSides.solve(list, 81));
     }
 
+    /**
+     *
+     * First iterate B elements and find sum of those
+     * then iterate from end, and while iterating add the last
+     * element and subtract the kth (start from B to 0) element
+     *
+     * @param A integer array list
+     * @param B number of elements to be picked
+     * @return maximum sum
+     */
     public int solve(ArrayList<Integer> A, int B) {
         int sum = 0;
         for(int i=B-1;i>=0;i--) {
