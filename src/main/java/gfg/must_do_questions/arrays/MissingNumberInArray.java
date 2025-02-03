f9b31d8062937625cbbea00f121bf95104eccd37
agarwal1995 <rahulagarwal199525@gmail.com>
2025-02-03 11:51:30 +0530

    sd

--- src/main/java/gfg/must_do_questions/arrays/MissingNumberInArray.java
+++ src/main/java/gfg/must_do_questions/arrays/MissingNumberInArray.java
@@ -8,7 +8,14 @@ import java.util.Arrays;
 public class MissingNumberInArray {
 
     // Note that the size of the array is n-1
-    int missingNumber(int n, int[] arr) {
+    int missingNumber(int[] arr) {
+        int n = 0;
+        for(int x:arr) {
+            n = Math.max(x, n);
+        }
+        if(n==arr.length) {
+            return n+1;
+        }
         long sumToNTerms = ((long) n *( n+1))/2;
         long sumOfArray = Arrays.stream(arr).sum();
         return (int)(sumToNTerms-sumOfArray);
